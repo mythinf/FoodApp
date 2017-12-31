3f01a00cc76444795e92abd3f94047e328110ba8
MYTH-INF <maytheinfante96@gmail.com>
2017-12-31 18:09:29 -0500

    adding functionalities for adding images

--- js/data.js
+++ js/data.js
@@ -1,72 +1,25 @@
-// var data =
-//   {
-//     'distritos':
-//     {
-//       'ANCON': {
-//         'top': [
-//           {
-//             'nombre': 'Delibakery',
-//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-//             'precio': 'S/.40'
-//           },
-//           {
-//             'nombre': 'La Quinta',
-//             'direccion': 'Jr. Loreto 615, Ancón',
-//             'precio': 'S/.45'
-//           }
-//         ],
-//         'cevicheria': [
-//           {
-//             'nombre': 'Delibakery',
-//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-//             'precio': 'S/.40'
-//           },
-//           {
-//             'nombre': 'Delibakery',
-//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-//             'precio': 'S/.40'
-//           },
-//           {
-//             'nombre': 'Delibakery',
-//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-//             'precio': 'S/.40'
-//           }
-//         ]
-//       },
-//       'ATE': {
-//         'top': [
-//           {
-//             'nombre': 'Delibakery',
-//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-//             'precio': 'S/.40'
-//           },
-//           {
-//             'nombre': 'La Quinta',
-//             'direccion': 'Jr. Loreto 615, Ancón',
-//             'precio': 'S/.45'
-//           }
-//         ],
-//         'cevicheria': [
-//           {
-//             'nombre': 'Delibakery',
-//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-//             'precio': 'S/.40'
-//           },
-//           {
-//             'nombre': 'Delibakery',
-//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-//             'precio': 'S/.40'
-//           },
-//           {
-//             'nombre': 'Delibakery',
-//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-//             'precio': 'S/.40'
-//           }
-//         ]
-//       }
-//
-//     }
-//   };
+var ancon =
+   {
+     'top': [
+        {
+          'nombre': 'Delibakery',
+          'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+          'precio': 'S/.40'
+        },
+        {
+          'nombre': 'La Quinta',
+          'direccion': 'Jr. Loreto 615, Ancón',
+          'precio': 'S/.45'
+        }
+      ],
+      'cevicheria': [
+        {
+          'nombre': 'Delibakery',
+          'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+          'precio': 'S/.40'
+        },
+      ]
+    };
 
 // var data = [
 //   {'BARRANCO':'restaurants'},
@@ -91,8 +44,8 @@
 // ];
 
 var distritos = [
-  'BARRANCO',
-  'BREÑA',
+  'ANCON',
+  'ATE',
   'CARABAYLLO',
   'CHORRILLOS',
   'COMAS',
--- js/home.js
+++ js/home.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
   var buttonSearch = $('button[type="submit"]');
+  var optionsFood = $('.options-food');
+  var topAncon = ancon.top;
+  var cevicheriaAncon = ancon.cevicheria;
+  var topImage = $('#foodTop');
+
   //  FUNCION QUE RECONOCE EL DISTRITO QUE DIGITE EL USUARIO Y ACTIVA EL BOTON DE BUSQUEDA
   $('input').keydown(function() {
     for (var i in distritos) {
+      // CONDICION : PARA BUSQUEDAS IGUALES A LA DATA
       if ($(this).val().toUpperCase() === distritos[i]) {
         buttonSearch.attr('disabled', false);
+        optionsFood.addClass('red');
+      // CONDICION : NUEVAS BÚSQUEDAS (SI EL USUARIO BORRA BUSQUEDA)
+      } else if ($(this).val().toUpperCase() === '') {
+        buttonSearch.attr('disabled', true);
+        optionsFood.removeClass('red');
       }
     }
-
-
-    // var places = distritos; // array del archivo data.js
-    // for (var i in places) {
-    //   if (searchPlace === places[i]) {
-    //     console.log(places[i]);
-    //   } else {
-    //     console.log('oh,no!');
-    //  }
-    //}
+    // FUNCIÓN QUE AGREGA IMAGENES EN CADA OPCION DE COMIDA
+    buttonSearch.click(function(){
+      topImage.append($(''))
+    })
   });
 });

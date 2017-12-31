a513cfeecc8d9492d14ad02598d35e16628f5b3f
MYTH-INF <maytheinfante96@gmail.com>
2017-12-31 14:25:15 -0500

    adding functionalities for button and input

--- js/data.js
+++ js/data.js
@@ -1,85 +1,113 @@
-var data = {
-  'ANCON': {
-    'top': [
-      {
-        'nombre': 'Delibakery',
-        'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-        'precio': 'S/.40'
-      },
-      {
-        'nombre': 'La Quinta',
-        'direccion': 'Jr. Loreto 615, Ancón',
-        'precio': 'S/.45'
-      }
-    ],
-    'cevicheria': [
-      {
-        'nombre': 'Delibakery',
-        'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-        'precio': 'S/.40'
-      },
-      {
-        'nombre': 'Delibakery',
-        'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-        'precio': 'S/.40'
-      },
-      {
-        'nombre': 'Delibakery',
-        'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-        'precio': 'S/.40'
-      }
-    ]
-  },
-  'ATE': {
-    'top': [
-      {
-        'nombre': 'Delibakery',
-        'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-        'precio': 'S/.40'
-      },
-      {
-        'nombre': 'La Quinta',
-        'direccion': 'Jr. Loreto 615, Ancón',
-        'precio': 'S/.45'
-      }
-    ],
-    'cevicheria': [
-      {
-        'nombre': 'Delibakery',
-        'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-        'precio': 'S/.40'
-      },
-      {
-        'nombre': 'Delibakery',
-        'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-        'precio': 'S/.40'
-      },
-      {
-        'nombre': 'Delibakery',
-        'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-        'precio': 'S/.40'
-      }
-    ]
-  }
-  // 'BARRANCO'
-  // 'BREÑA'
-  // 'CARABAYLLO'
-  // 'CHORRILLOS'
-  // 'COMAS'
-  // 'EL AGUSTINO'
-  // 'INDEPENCIA'
-  // 'JESUS MARIA'
-  // 'LA MOLINA'
-  // 'LA VICTORIA'
-  // 'LIMA'
-  // 'LINCE'
-  // 'LOS OLIVOS'
-  // 'MAGDALENA DE MAR'
-  // 'MIRAFLORES'
-  // 'PUEBLO LIBRE'
-  // 'RIMAC'
-  // 'SAN MIGUEL'
+// var data =
+//   {
+//     'distritos':
+//     {
+//       'ANCON': {
+//         'top': [
+//           {
+//             'nombre': 'Delibakery',
+//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//             'precio': 'S/.40'
+//           },
+//           {
+//             'nombre': 'La Quinta',
+//             'direccion': 'Jr. Loreto 615, Ancón',
+//             'precio': 'S/.45'
+//           }
+//         ],
+//         'cevicheria': [
+//           {
+//             'nombre': 'Delibakery',
+//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//             'precio': 'S/.40'
+//           },
+//           {
+//             'nombre': 'Delibakery',
+//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//             'precio': 'S/.40'
+//           },
+//           {
+//             'nombre': 'Delibakery',
+//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//             'precio': 'S/.40'
+//           }
+//         ]
+//       },
+//       'ATE': {
+//         'top': [
+//           {
+//             'nombre': 'Delibakery',
+//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//             'precio': 'S/.40'
+//           },
+//           {
+//             'nombre': 'La Quinta',
+//             'direccion': 'Jr. Loreto 615, Ancón',
+//             'precio': 'S/.45'
+//           }
+//         ],
+//         'cevicheria': [
+//           {
+//             'nombre': 'Delibakery',
+//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//             'precio': 'S/.40'
+//           },
+//           {
+//             'nombre': 'Delibakery',
+//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//             'precio': 'S/.40'
+//           },
+//           {
+//             'nombre': 'Delibakery',
+//             'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//             'precio': 'S/.40'
+//           }
+//         ]
+//       }
+//
+//     }
+//   };
 
+// var data = [
+//   {'BARRANCO':'restaurants'},
+//   {'distrito': 'BREÑA'},
+//   {'distrito': 'CARABAYLLO'},
+//   {'distrito': 'CHORRILLOS'},
+//   {'distrito': 'COMAS'},
+//   {'distrito': 'EL AGUSTINO'},
+//   {'distrito': 'INDEPENCIA'},
+//   {'distrito': 'JESUS MARIA'},
+//   {'distrito': 'LA MOLINA'},
+//   {'distrito': 'BREÑA'},
+//   {'distrito': 'BREÑA'},
+//   {'distrito': 'LA VICTORIA'},
+//   {'distrito': 'LIMA'},
+//   {'distrito': 'LINCE'},
+//   {'distrito': 'LOS OLIVOS'},
+//   {'distrito': 'MAGDALENA DE MAR'},
+//   {'distrito': 'PUEBLO LIBRE'},
+//   {'distrito': 'RIMAC'},
+//   {'distrito': 'SAN MIGUEL'},
+// ];
 
-
-}
+var distritos = [
+  'BARRANCO',
+  'BREÑA',
+  'CARABAYLLO',
+  'CHORRILLOS',
+  'COMAS',
+  'EL AGUSTINO',
+  'INDEPENCIA',
+  'JESUS MARIA',
+  'LA MOLINA',
+  'BREÑA',
+  'BREÑA',
+  'LA VICTORIA',
+  'LIMA',
+  'LINCE',
+  'LOS OLIVOS',
+  'MAGDALENA DE MAR',
+  'PUEBLO LIBRE',
+  'RIMAC',
+  'SAN MIGUEL',
+];
--- js/home.js
+++ js/home.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  var placeInput = $('input');
-  //  FUNCION QUE RECONOCE EL DISTRITO QUE DIGITE EL USUARIO
-  placeInput.keypress(function() {
-    console.log(placeInput.val().toUpperCase());
+  var buttonSearch = $('button[type="submit"]');
+  //  FUNCION QUE RECONOCE EL DISTRITO QUE DIGITE EL USUARIO Y ACTIVA EL BOTON DE BUSQUEDA
+  $('input').keydown(function() {
+    for (var i in distritos) {
+      if ($(this).val().toUpperCase() === distritos[i]) {
+        buttonSearch.attr('disabled', false);
+      }
+    }
+
+
+    // var places = distritos; // array del archivo data.js
+    // for (var i in places) {
+    //   if (searchPlace === places[i]) {
+    //     console.log(places[i]);
+    //   } else {
+    //     console.log('oh,no!');
+    //  }
+    //}
   });
 });

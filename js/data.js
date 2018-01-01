b32740463ac58df86d014f4abe681dd85d5647ce
MYTH-INF <maytheinfante96@gmail.com>
2018-01-01 12:43:12 -0500

    adding more options data.js

--- js/data.js
+++ js/data.js
@@ -1,22 +1,87 @@
+// ZONAS DE LIMA
 var zonas = [
   'NORTE',
   'CENTRO',
   'SUR',
 ];
-
+// RESTAURANTES-IMG TOP NORTE-CENTRO-SUR
 var topNorte = [
   '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
   '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
 ];
 var topCentro = [
-  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
-  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+  '<img src="../assets/img/puerto-norte.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/san-martin.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/chifa-joyLao.jpg" class="img-responsive img-thumbnail" alt="i">',
 ];
 var topSur = [
+  '<img src="../assets/img/limon-sal.jpg" class="img-responsive img-thumbnail" alt="i">',
+];
+// RESTAURANTES-IMG CEVICHEIAS NORTE-CENTRO-SUR
+var cevicheriaNorte = [
   '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
   '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
 ];
-
+var cevicheriaCentro = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var cevicheriaSur = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+// RESTAURANTES-IMG CRIOLLOS NORTE-CENTRO-SUR
+var criolloNorte = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var criolloCentro = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var criolloSur = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+// RESTAURANTES-IMG ECONOMICOS NORTE-CENTRO-SUR
+var econNorte = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var econCentro = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var econSur = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+// RESTAURANTES-IMG FUSION NORTE-CENTRO-SUR
+var fusionNorte = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var fusionCentro = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var fusionSur = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+// RESTAURANTES-IMG CARNES Y PARRILLAS NORTE-CENTRO-SUR
+var carnespNorte = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var carnespCentro = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var carnespSur = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
 // var data = {
 //   'zonas': [
 //     {

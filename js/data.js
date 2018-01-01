a26f773251b1aca23b9bffd3f7b666d6b0a100e1
MYTH-INF <maytheinfante96@gmail.com>
2018-01-01 11:58:30 -0500

    adding zones,img-restaurant and interaction for zone north

--- js/data.js
+++ js/data.js
@@ -1,59 +1,78 @@
-var data = {
-  'zonas': [
-    {
-      'lima norte': [
-        {
-          'top': [
-            {
-              'image': '<img src="../assets/img/deli-bakery.jpg" alt="i">',
-              'nombre': 'Delibakery',
-              'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-              'precio': 'S/.40',
-            },
-            {
-              'image': '<img src="../assets/img/la-quinta.jpg" alt="i">',
-              'nombre': 'La Quinta',
-              'direccion': 'Jr. Loreto 615, Ancón',
-              'precio': 'S/.45'
-            }
-          ],
-          'cevicheria': [
-            {
-              'nombre': 'Delibakery',
-              'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-              'precio': 'S/.40'
-            },
-          ]
-        }
-      ],
-      'lima sur': [
-        {
-          'top': [
-            {
-              'image': '<img src="../assets/img/deli-bakery.jpg" alt="i">',
-              'nombre': 'Delibakery',
-              'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-              'precio': 'S/.40',
-            },
-            {
-              'image': '<img src="../assets/img/la-quinta.jpg" alt="i">',
-              'nombre': 'La Quinta',
-              'direccion': 'Jr. Loreto 615, Ancón',
-              'precio': 'S/.45'
-            }
-          ],
-          'cevicheria': [
-            {
-              'nombre': 'Delibakery',
-              'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
-              'precio': 'S/.40'
-            },
-          ]
-        }
-      ]
-    }
-  ]
-};
+var zonas = [
+  'NORTE',
+  'CENTRO',
+  'SUR',
+];
+
+var topNorte = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var topCentro = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+var topSur = [
+  '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+  '<img src="../assets/img/la-quinta.jpg" class="img-responsive img-thumbnail" alt="i">'
+];
+
+// var data = {
+//   'zonas': [
+//     {
+//       'lima norte': [
+//         {
+//           'top': [
+//             {
+//               'image': '<img src="../assets/img/deli-bakery.jpg" class="img-responsive img-thumbnail" alt="i">',
+//               'nombre': 'Delibakery',
+//               'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//               'precio': 'S/.40',
+//             },
+//             {
+//               'image': '<img src="../assets/img/la-quinta.jpg" alt="i">',
+//               'nombre': 'La Quinta',
+//               'direccion': 'Jr. Loreto 615, Ancón',
+//               'precio': 'S/.45'
+//             }
+//           ],
+//           'cevicheria': [
+//             {
+//               'nombre': 'Delibakery',
+//               'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//               'precio': 'S/.40'
+//             },
+//           ]
+//         }
+//       ],
+//       'lima sur': [
+//         {
+//           'top': [
+//             {
+//               'image': '<img src="../assets/img/deli-bakery.jpg" alt="i">',
+//               'nombre': 'Delibakery',
+//               'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//               'precio': 'S/.40',
+//             },
+//             {
+//               'image': '<img src="../assets/img/la-quinta.jpg" alt="i">',
+//               'nombre': 'La Quinta',
+//               'direccion': 'Jr. Loreto 615, Ancón',
+//               'precio': 'S/.45'
+//             }
+//           ],
+//           'cevicheria': [
+//             {
+//               'nombre': 'Delibakery',
+//               'direccion': 'Malecón Pardo, La Rotonda, Ancón ',
+//               'precio': 'S/.40'
+//             },
+//           ]
+//         }
+//       ]
+//     }
+//   ]
+// };
 
 // var data = [
 //   {'BARRANCO':'restaurants'},
@@ -76,9 +95,3 @@ var data = {
 //   {'distrito': 'RIMAC'},
 //   {'distrito': 'SAN MIGUEL'},
 // ];
-
-var zonas = [
-  'NORTE',
-  'CENTRO',
-  'SUR',
-];
--- js/home.js
+++ js/home.js
@@ -1,38 +1,22 @@
 $(document).ready(function() {
-  // evento mouse over para los mapas de las zonas de lima
-  $('#img-mapa1').mouseover(function(){
-    $(this).hide();
-  });
-  $('#img-mapa2').mouseover(function(){
-    $(this).hide();
-  });
-  $('#img-mapa3').mouseover(function(){
-    $(this).hide();
-  });
-
   // variable que alacena datos de las diferentes opciones de comida (DOM)
-   var optionsFood = $('.options-food');
-  // // variable que almacena datos de los restaurants tops de ANCON (data.js)
-  // var limaNorte = data['zonas'];
-  //
-  // // variable que almacena datos de las cevicherias de ANCON (data.js)
-  // var cevicheriaAncon = ancon.cevicheria;
-  // // variable que almacenará las imágenes de cada restaurant TOP (DOM)
-  // var topImage = $('#foodTop');
+  var optionsFood = $('.options-food');
+  // variable que almacenará las imágenes de cada restaurant TOP (DOM)
+  var topImage = $('#foodTop');
   //
   //  FUNCION QUE RECONOCE EL DISTRITO QUE DIGITE EL USUARIO Y ACTIVA EL BOTON DE BUSQUEDA
   $('input').keydown(function() {
     for (var i in zonas) {
-      // CONDICION : PARA BUSQUEDAS IGUALES A LA DATA
-      if ($(this).val().toUpperCase() === zonas[i]) {
+      // CONDICION : PARA BUSQUEDAS PARA ZONA NORTE
+      if ($(this).val().toUpperCase() === zonas[0]) {
         optionsFood.addClass('red');
+        $('#top').click(function() {
+          topImage.append(topNorte);
+        });
       // CONDICION : NUEVAS BÚSQUEDAS (SI EL USUARIO BORRA BUSQUEDA)
       } else if ($(this).val().toUpperCase() === '') {
         optionsFood.removeClass('red');
       }
     }
-
   });
-  // // FUNCIÓN QUE AGREGA IMAGENES EN CADA OPCION DE COMIDA
-  //   topImage.append(ancon.top[0].image);
 });
